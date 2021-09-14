3fc759559b02a9c4ed4ac5e4168e3a3c19c02eb6
Cai <trieungochai.dev@gmail.com>
2021-09-14 23:09:24 +0700

    Creating Your First EJS Templates

--- app.js
+++ app.js
@@ -2,20 +2,43 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+
+app.set("view-engine", "ejs");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   var today = new Date();
-  var currentDay = today.getDate();
+  var currentDay = today.getDay();
+  var day = "";
 
-  if (currentDay === 6 || currentDay === 0) {
-    res.write("<h1>Yay it's the weekend</h1>");
-  } else {
-    // res.write("<p>It is not the weekend!</p>");
-    // res.write("<h1>Boo! I have to work!!!</h1>");
-    res.sendFile(__dirname + "/index.html");
-    // res.send();
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      console.log("Error: current day is equal to:" + currentDay);
   }
+
+  res.render("list.ejs", { kindOfDay: day });
 });
 
 app.listen(3000, function () {
