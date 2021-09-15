0b125e3ac75d37823dd256f0d96e2d4acd0a0863
Cai <trieungochai.dev@gmail.com>
2021-09-15 15:57:27 +0700

    Adding Pre-Made CSS Stylesheets to Your Website

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 app.set("view-engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 var items = ["Buy Food", "Cook Food", "Eat Food"];
 
