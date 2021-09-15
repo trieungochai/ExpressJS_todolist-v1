0107f3d83e9710d98e366f8cbbae380b4954c0e0
Cai <trieungochai.dev@gmail.com>
2021-09-15 16:27:51 +0700

    Understanding Templating vs. Layouts

--- app.js
+++ app.js
@@ -8,27 +8,42 @@ app.set("view-engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.get("/", function (req, res) {
-  var today = new Date();
-  var options = {
+  let today = new Date();
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long",
   };
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list.ejs", { kindOfDay: day, newListItems: items });
+  res.render("list.ejs", { listTitle: day, newListItems: items });
 });
 
 app.post("/", function (req, res) {
-  var item = req.body.newItem;
+  let item = req.body.newItem;
 
-  items.push(item);
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
 
-  res.redirect("/");
+app.get("/work", function (req, res) {
+  res.render("list.ejs", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", function () {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
 });
 
 app.listen(3000, function () {
