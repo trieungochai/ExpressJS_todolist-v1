4fa566201c7f035189bc93b3ce75c514b76e6396
Hai Trieu (BnK - HCM) <hai.trieu@bnksolution.com>
2021-09-22 11:30:01 +0700

    Connecting the TodoList with Mongoose

--- app.js
+++ app.js
@@ -1,20 +1,48 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.set("view-engine", "ejs");
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+});
+
+const itemsSchema = new mongoose.Schema({
+  name: String,
+});
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todoList!",
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add a new item.",
+});
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item.",
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successfully saved default items to Database.");
+  }
+});
 
 app.get("/", function (req, res) {
-  const day = date.getDate();
-  res.render("list.ejs", { listTitle: day, newListItems: items });
+  res.render("list", { listTitle: "Today", newListItems: items });
 });
 
 app.post("/", function (req, res) {
@@ -30,7 +58,7 @@ app.post("/", function (req, res) {
 });
 
 app.get("/work", function (req, res) {
-  res.render("list.ejs", { listTitle: "Work List", newListItems: workItems });
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
 app.post("/work", function () {
